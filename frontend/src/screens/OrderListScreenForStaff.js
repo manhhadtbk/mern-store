f5d3d9ee7582f5c1e4888884929d6e0fae433793
manhhadtbk <56177308+manhhadtbk@users.noreply.github.com>
2022-07-31 16:15:32 +0700

    hide name of user when show in orderlist scren

--- frontend/src/screens/OrderListScreenForStaff.js
+++ frontend/src/screens/OrderListScreenForStaff.js
@@ -103,7 +103,7 @@ export default function OrderScreenListForStaff() {
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
-                <td>{order.user ? order.user.name : 'DELETE USER'}</td>
+                <td>{order.user ? order.user.name : '' /* 'DELETED USER' */}</td>
                 <td>{order.createdAt.substring(0, 10)}</td>
                 <td>{order.totalPrice.toFixed(2)}</td>
                 {order.isPaid ?
